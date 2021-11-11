c7282bb53ccdf64215f4ea76867cb31723904c4c
seriouslysexy <erivermg@hotmail.com>
2021-11-10 23:07:17 -0500

    this time now actually paying attention to the layout

--- src/components/layout/LayoutContainer.jsx
+++ src/components/layout/LayoutContainer.jsx
@@ -2,41 +2,47 @@ import React from "react";
 
 import Box from "@mui/material/Box";
 
-// import { makeStyles } from '@mui/styles';
+import { makeStyles } from '@mui/styles';
 
 import ProfilePicture from "../profilePicture/ProfilePicture";
 import Introduction from "../introduction";
 import Skills from "../skills";
 
-// const useStyles = makeStyles({
-// 	outerContentContainerStyles: {
-// 		height: "100%",
-// 		width: "100%",
-// 		display: "block",
-// 		position: "relative",
-// 		textAlign: "center"
-// 	},
-// 	innerContentContainer: {
-// 		height: "100%",
-// 		width: "80%",
-// 		maxWidth: "1400px",
-// 		display: "block",
-// 		position: "relative",
-// 	}
-// });
+const useStyles = makeStyles({
+	leftContent: {
+		height: "100%",
+		paddingRight: "50px",
+		position: "relative",
+		"&::before": {
+			content: "''",
+			display: "block",
+			position: "absolute",
+			height: "200%",
+			top: "-50%",
+			width: "100vh",
+			right: "0",
+			background: "rgba(255, 255, 255, .3)",
+			zIndex: "-1"
+		}
+	},
+	rightContent: {
+		height: "100%",
+		paddingLeft: "50px"
+	}
+});
 
 function LayoutContainer() {
-	// const classes = useStyles();
+	const classes = useStyles();
 
 	return (
-		<Box height="100%" width="100%" display="flex" flexDirection="row" justifyContent="center" alignItems="flex-start">
+		<Box p="10vh" height="100%" width="100%" display="flex" flexDirection="row" justifyContent="center" alignItems="flex-start">
 			<Box height="100%" width="80%" maxWidth="1200px" display="flex" flexDirection="row" justifyContent="center" alignItems="flex-start">
-				<Box className="leftContent">
+				<Box className={classes.leftContent}>
 					<ProfilePicture />
 					<div>navigation?</div>
 					<div>contact info</div>
 				</Box>
-				<Box className="rightContent">
+				<Box className={classes.rightContent}>
 					<Introduction />
 					<Skills />
 					<div>professional introduction</div>
