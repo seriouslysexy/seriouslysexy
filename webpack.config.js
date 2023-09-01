9c7676e81ae96b8d650be42f7c25975f066d205e
seriouslysexy <erivermg@hotmail.com>
2023-09-01 00:12:10 -0700

    remove last class component, add favicon and meta description, remove extra script call

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => ({
   entry: "./src/index.js",
@@ -9,13 +10,13 @@ module.exports = (env, argv) => ({
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         loader: "babel-loader",
         options: { presets: ["@babel/env"] }
       },
       {
         test: /\.(jpg|json)$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         type: "asset/resource"
       },
       {
@@ -44,6 +45,12 @@ module.exports = (env, argv) => ({
 			template: "src/index.ejs", // these are default values
       filename: "index.html", // these are default values
       templateParameters: { env, argv } // passing in everything from the command-line
-		})
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/static/img/favicon-*", to: "[name].[ext]" },
+        { from: "src/robots.txt", to: "[name][ext]" }
+      ]
+    })
 	]
 });
